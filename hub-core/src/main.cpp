205d8b54fa74b76c94c0ee515b08d6aa8ae74b91
FlorianBrett <florian.brett@gmail.com>
2016-03-08 16:19:22 +0000

    Hub Core Project created

---
+++ hub-core/src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Home Automation Hub Core
+ *      Author: Florian Brett
+ */
+#include <string>
+#include <iostream>
+#include <thread>
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+int main(int argc, char* argv[])
+{
+	std::cout << "hello world";
+}
